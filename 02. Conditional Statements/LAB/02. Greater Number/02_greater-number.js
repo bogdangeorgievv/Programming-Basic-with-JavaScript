53fc09af64abf33670478dae3d570a08ae43b5fe
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2023-07-26 12:55:07 +0300

    Create 02_greater-number.js

---
+++ 02. Conditional Statements/LAB/02. Greater Number/02_greater-number.js
@@ -0,0 +1,10 @@
+function greater(input) {
+  let a = Number(input[0]);
+  let b = Number(input[1]);
+  if (a > b) {
+    console.log(a);
+  } else {
+    console.log(b);
+  }
+}
+greater([`5`, `5`]);
