92ce6a86bc0731171cb6485aadcf2d1b3112506f
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2023-07-27 18:06:53 +0300

    Create 02_bonus-points.js

---
+++ 02. Conditional Statements/Exercise/02. Bonus Points/02_bonus-points.js
@@ -0,0 +1,22 @@
+function bonusPoints(input) {
+    
+  let a = Number(input[0]);
+  let bonus = 0;
+
+  if (a <= 100) {
+    bonus = 5;
+  } else if (a > 1000) {
+    bonus = a * 0.1;
+  } else {
+    bonus = a * 0.2;
+  }
+
+  if (a % 2 == 0) {
+    bonus += 1;
+  } else if (a % 10 == 5) {
+    bonus = bonus + 2;
+  }
+  console.log(bonus);
+  console.log(a + bonus);
+}
+bonusPoints([`20`]);
