d0d337ac03883f533ade084add4b07b815fb6b45
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2023-08-03 20:00:58 +0300

    Create 10_invalid-number.js

---
+++ 03. Nested Conditional Statements/LAB/10. Invalid Number/10_invalid-number.js
@@ -0,0 +1,9 @@
+function check(input) {
+  let number = Number(input[0]);
+  let isValid = (number >= 100) & (number <= 200) || number == 0;
+
+  if (!isValid) {
+    console.log("invalid");
+  }
+}
+check([150]);
