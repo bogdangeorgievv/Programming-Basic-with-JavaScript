57c0040a7ef93969d9c8dbc81149065d04bbed2f
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2023-08-14 17:46:12 +0300

    Create 01_words-read.js

---
+++ 05. While Loop/LAB/01. Words Read/01_words-read.js
@@ -0,0 +1,30 @@
+function masiv(input) {
+  let index = 0;
+  while (true) {
+    let str = input[index];
+    index++;
+    if (str == "Stop") {
+      break;
+    }
+    console.log(str);
+  }
+}
+masiv([
+  "Nakov",
+
+  "SoftUni",
+
+  "Sofia",
+
+  "Bulgaria",
+
+  "SomeText",
+
+  "Stop",
+
+  "AfterStop",
+
+  "Europe",
+
+  "HelloWorld",
+]);
