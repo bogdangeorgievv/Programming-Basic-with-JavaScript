ec4970b0842c95a87c94a8747b58ce45bb3b25ba
Bogdan Georgiev <140177649+bogdangeorgievv@users.noreply.github.com>
2023-09-05 19:21:21 +0300

    Create 05_travelling-ani.js

---
+++ 06. Nested Loops/LAB/05. Travelling Ani/05_travelling-ani.js
@@ -0,0 +1,40 @@
+function traveling(input) {
+
+    let command = input[0];
+    let index = 1;
+
+    while(command !== "End") {
+
+        let destination = command;
+        let budget = Number(input[index]);
+        index++;
+
+        let moneySaved = 0;
+
+        while (moneySaved < budget) {
+            let currentBudget = Number(input[index]);
+            index++;
+            moneySaved += currentBudget;
+        }
+        console.log(`Going to ${destination}!`);
+        command = input[index];
+        index++;
+        
+    }
+
+}
+traveling(["Greece",
+"1000",
+"200",
+"200",
+"300",
+"100",
+"150",
+"240",
+"Spain",
+"1200",
+"300",
+"500",
+"193",
+"423",
+"End"])
